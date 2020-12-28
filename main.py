23c128c4a711e68eaec5cc6d913dea6a25ff17a0
youngdevps <jordiibayo@gmail.com>
2020-12-28 03:15:43 +0100

    add asynchronous programming

--- main.py
+++ main.py
@@ -1,25 +1,34 @@
 import flow_metter_control
 import time
 import os
+import signal
+import sys
+import  asyncio
 
 print("===================1================")
 import RPi.GPIO as GPIO
 GPIO.setwarnings(False)
 from mfrc522 import SimpleMFRC522
+flow_metter_control.setup()
 
 reader = SimpleMFRC522()
 on = False
 tagId = str()
 oldTagId = ""
 gapElapsedTime = 2.2
-flow_metter_control.setup()
 cloudWateQuantity = 0
 minValueToHaveToFectchWater = 1
 pulseFlow = 0
 
-while True:
+
+
+async def startByVerifiying():
+    globalVars = globals()
+    globalVars['reader', 'on', 'tagId', 'oldTagId', 'gapElapsedTime', 'cloudWateQuantity', 'minValueToHaveToFectchWater', 'pulseFlow']
     tagId = ""
     print("===================2================")
+
+
     if on == True:
         print("===================3================")
         if cloudWateQuantity <= pulseFlow:
@@ -30,9 +39,14 @@ while True:
             # TODO: save flow and count on firebase
             print("===================5================")
 
+async def getCardFirst():
+    globalVars = globals()
+    globalVars['reader', 'on', 'tagId', 'oldTagId', 'gapElapsedTime', 'cloudWateQuantity', 'minValueToHaveToFectchWater', 'pulseFlow']
+    tagId = ""
     print("===================6================")
     tagId, text = reader.read() 
     print("===================7================")
+
 
     if tagId != "":
         time.sleep(gapElapsedTime)
@@ -60,3 +74,16 @@ while True:
                 # TODO: show on screen that use dont have enought water flow to open tap
                 print("You dont have enought water flow")
                 print("===================16================")
+
+
+
+def signal_handler(signal, frame):
+    tagId = ""
+    loop.stop()
+    sys.exit(0)
+
+ listOfGlobals['total'] = 15
+signal.signal(signal.SIGINT, signal_handler)
+
+asyncio.ensure_future(startByVerifiying())
+asyncio.ensure_future(getCardFirst())
