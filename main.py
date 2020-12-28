a4c212c9dd566705f5a9291bf84da8084f62c816
youngdevps <jordiibayo@gmail.com>
2020-12-28 02:36:54 +0100

    new main

--- main.py
+++ main.py
@@ -7,68 +7,56 @@ import RPi.GPIO as GPIO
 GPIO.setwarnings(False)
 from mfrc522 import SimpleMFRC522
 
-print("===================2================")
 reader = SimpleMFRC522()
 on = False
-oldId = ""
-print("===================3================")
+tagId = str()
+oldTagId = ""
 gapElapsedTime = 2.2
 flow_metter_control.setup()
-
-"""
-while True:
-    print("===================4================")
-    try:
-        id, text = reader.read()
-        flow_metter_control.setup()
-        if on == True:
-            print("===================5================")
-            if str(oldId) == str(id):
-                print("===================6================")
-                on = False
-                # write the final flow to tag
-                flow_metter_control.stop_flow_counter()
-                print("===================7================")
-        else:
-            print("===================9================")
-            on = True
-            oldId = str(id)
-            flow_metter_control.start_flow_counter2()
-            print("===================10================")
-    except:
-        print("************* out of reading tag")
- 
-print("===================11================")  
-"""
+cloudWateQuantity = 0
+minValueToHaveToFectchWater = 1
+pulseFlow = 0
 
 while True:
-    print("===================4================")
-    id, text = reader.read()
-    print("===================the id is id_: {}================".format(id))
-    time.sleep(gapElapsedTime)
+    tagId = ""
+    print("===================2================")
     if on == True:
-        print("===================5================")
-        if str(oldId) == str(id):
-            print("===================6================")
-            on = False
-            # write the final flow to tag
-            count, flow = flow_metter_control.stop_flow_counter()
-            time.sleep(gapElapsedTime)
-            print("///////////count:{} and flow:{}/////////////////".format(count, flow))
-            flow_metter_control.resetCountAndFlow()
-            print("===================7================")
-    else:
+        print("===================3================")
+        if cloudWateQuantity <= pulseFlow:
+            print("===================4================")
+            count, pulseFlow = flow_metter_control.stop_flow_counter()
+            flow_metter_control.resetCountAndFlow() # set to 0 count and flow
+            print("::::::::::::: the flow is ", pulseFlow)
+            # TODO: save flow and count on firebase
+            print("===================5================")
+
+    print("===================6================")
+    tagId, text = reader.read() 
+    print("===================7================")
+
+    if tagId != "":
+        time.sleep(gapElapsedTime)
         print("===================8================")
-        on = True
-        oldId = str(id)
-        flow_metter_control.start_flow_counter2()
-        flowToReach = 1
-        hasReached = False
-        while hasReached is False:
-            count, flow = flow_metter_control.stop_flow_counter()
-            print("::::::::::::: the flow is ", flow)
-            if flow == flowToReach:
-                hasReached = True
-        print("===================9================")
- 
-print("===================10================")  
+        if on == True:
+            print("===================9================")
+            if tagId == oldTagId:
+                print("===================10================")
+                count, pulseFlow = flow_metter_control.stop_flow_counter()
+                flow_metter_control.resetCountAndFlow() # set to 0 count and flow
+                print("::::::::::::: the flow is ", pulseFlow)
+                # TODO: save flow and count on firebase
+                print("===================11================")
+        else:
+            print("===================12================")
+            # TODO: get the water value of the card from firebase using his tagId
+            cloudWateQuantity = 15 # i simmulate the fact that we already have the value
+            if cloudWateQuantity >= minValueToHaveToFectchWater:
+                print("===================13================")
+                oldTagId = tagId
+                flow_metter_control.start_flow_counter2()
+                print("===================14================")
+            else:
+                print("===================15================")
+                # TODO: show on screen that use dont have enought water flow to open tap
+                print("You dont have enought water flow")
+                print("===================16================")
